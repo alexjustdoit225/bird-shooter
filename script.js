93227b4da72dcaf4bfff7334229e3a5c638e797a
alexjustdoit225 <alex.minnie.aw@gmail.com>
2022-02-23 19:06:57 -0600

    Update script.js
    -setup script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+const canvas = document.getElementById('canvas1'); 
+const c = canvas.getContext('2d'); 
+const  CANVAS_WIDTH = canvas.width = 600; 
+const CANVAS_HEIGHT = canvas.height = 600; 
+
+class Ravens {
+    constructor(){
+        this.x; 
+        this.y; 
+        this.spriteWidth; 
+        this.spriteHeight; 
+        this.frame; 
+        this.speed; 
+        this.width; 
+        this.height; 
+    }
+}
