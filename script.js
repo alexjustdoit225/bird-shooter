0a6b6c2b2f7500a31686b6844e130d2b183ea118
alexjustdoit225 <alex.minnie.aw@gmail.com>
2022-02-23 19:19:29 -0600

    Update script.js
    -added update function
    -added animate function
    -added draw function

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/** @type {HTMLCanvasElement} */
 const canvas = document.getElementById('canvas1'); 
 const c = canvas.getContext('2d'); 
 const  CANVAS_WIDTH = canvas.width = 600; 
@@ -6,13 +7,30 @@ const CANVAS_HEIGHT = canvas.height = 600;
 let birds = []; 
 class Bird {
     constructor(){
-        this.x; 
-        this.y; 
+        this.x = canvas.width; 
+        this.y = Math.random() * (canvas.height - this.height); 
+        this.directionX = Math.random() * 5 + 3; 
+        this.directionY = Math.random() * 5 - 2.5; 
         this.spriteWidth; 
         this.spriteHeight; 
-        this.frame; 
+        this.frame = 0; 
         this.speed; 
-        this.width; 
-        this.height; 
+        this.width = 100; 
+        this.height = 50;
+
+    }
+    update(){
+        this.x -= this.directionX; 
+        this.frame++; 
+    }
+    draw(){
+        c.fillRect(this.x, this.y, this.width, this.height);
     }
 }
+function animate(){
+    c.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT); 
+    // bird.draw();
+    // bird.update(); 
+    requestAnimationFrame(animate); 
+}
+animate(); 
